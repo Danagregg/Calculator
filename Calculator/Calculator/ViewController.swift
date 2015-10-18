99a523722e834a65c72fbdf07c49550db3911220
Dana Gregg <dana.gregg@hootsuite.com>
2015-10-18 16:49:25 -0700

    Calculator Initialize
    Basic calculator with buttons and view

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dana Gregg on 10/18/15.
+//  Copyright © 2015 Dana Gregg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+  
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+
+
+}
+
